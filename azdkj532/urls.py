113cdb42af8ad7ef7b5c17be639a78f6e59e30dc
azdkj532 <kanade123321@gmail.com>
2014-07-21 15:31:09 +0800

    little change

--- azdkj532/urls.py
+++ azdkj532/urls.py
@@ -7,7 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'azdkj532.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/?', include(admin.site.urls)),
     url(r'^.*$',  'dmhy.views.index' ),
 )
