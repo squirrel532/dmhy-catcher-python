1804da50ff3f864bee680182c7affef34ef32e43
azdkj532 <kanade123321@gmail.com>
2014-07-22 00:13:17 +0800

    Delete garbage file

--- dmhy/cron.py
+++
@@ -1,9 +0,0 @@
-from django_cron import CronJobBase, Schedule
-class MyCronJob( CronJobBase ):
-    ALLOW_PARALLEL_RUNS = True
-    RUN_EVERY_MINS = 1
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'dmhy.cronjob'
-    
-    def do(self):
-        print "cron test"
