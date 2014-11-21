b92e08cf2dd4e30bb056b12759e7837c8e8c33a0
azdkj532 <kanade123321@gmail.com>
2014-11-21 14:50:14 +0800

    remove perfix

--- azdkj532/urls.py
+++ azdkj532/urls.py
@@ -7,10 +7,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^dmhy/admin/', include(admin.site.urls)),
-    url(r'^dmhy/history/',  'dmhy.views.history' ),
-    url(r'^dmhy/$',  'dmhy.views.index' ),
-    url(r'^dmhy/api/tasklist/', 'dmhy.views.tasklist'),
-    url(r'^dmhy/api/resourcelist/(?P<tid>[0-9]*)', 'dmhy.views.resourcelist'),
-    url(r'^dmhy/api/history/', 'dmhy.views.records'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^history/',  'dmhy.views.history' ),
+    url(r'^$',  'dmhy.views.index' ),
+    url(r'^api/tasklist/', 'dmhy.views.tasklist'),
+    url(r'^api/resourcelist/(?P<tid>[0-9]*)', 'dmhy.views.resourcelist'),
+    url(r'^api/history/', 'dmhy.views.records'),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
