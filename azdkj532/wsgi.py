15880e584040341fdb649ba83e165bc467220eb5
azdkj532 <kanade123321@gmail.com>
2014-11-21 14:50:43 +0800

    add to sys.path for apache

--- azdkj532/wsgi.py
+++ azdkj532/wsgi.py
@@ -10,5 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "azdkj532.settings")
 
+import sys
+sys.path.append('/home/kuroneko/dmhy-catcher-python')
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
