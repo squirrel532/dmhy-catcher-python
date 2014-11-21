c98f6ea09dd7f1ac4af0a2f33b900fad38052986
azdkj532 <kanade123321@gmail.com>
2014-11-21 14:54:07 +0800

    remove two url

--- azdkj532/urls.py
+++ azdkj532/urls.py
@@ -8,8 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^history/',  'dmhy.views.history' ),
-    url(r'^$',  'dmhy.views.index' ),
     url(r'^api/tasklist/', 'dmhy.views.tasklist'),
     url(r'^api/resourcelist/(?P<tid>[0-9]*)', 'dmhy.views.resourcelist'),
     url(r'^api/history/', 'dmhy.views.records'),
