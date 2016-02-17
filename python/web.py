64ece41ab8265985ea1383220609b77e3bfbabf2
Squirrel <kanade123321@gmail.com>
2016-02-18 00:37:10 +0800

    add searching api

--- python/web.py
+++ python/web.py
@@ -1,7 +1,8 @@
 import json
-from bottle import Bottle, run
+from bottle import Bottle, run, request
 
 from db import Task, Resource
+from dmhy import dmhy
 
 app = Bottle()
 
@@ -9,12 +10,20 @@ app = Bottle()
 @app.route('/task')
 def task_list():
     tasks = [task for task in Task.select().dicts()]
-    return json.dumps({"task_list": tasks})
+    return json.dumps({"task_list": tasks}, ensure_ascii=False)
 
 
 @app.route('/resource')
 def resource_list():
     resources = [_ for _ in Resource.select().dicts()]
-    return json.dumps({"resource": resources})
+    return json.dumps({"resource": resources}, ensure_ascii=False)
+
+
+@app.route('/search')
+def search():
+    keyword = request.query.get('q', '')
+    res = [{"title": topic.title, "last_update": topic.date} for topic in dmhy.Search(keyword)]
+    return json.dumps({"resource": res}, ensure_ascii=False)
+
 
 run(app, host='localhost', port=8080)
